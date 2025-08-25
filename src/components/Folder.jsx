f3eea41bfad8616a13bfa8de19fc3344fd382428
correo940 <todojuntomirar@gmail.com>
2025-08-25 02:54:31 +0200

    fix: resuelve conflictos en Folder.jsx

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -3,14 +3,10 @@ import React from 'react';
 const Folder = ({ name }) => {
   return (
     <div className="folder">
-<<<<<<< HEAD
       <div className="folder-content">
         <span className="folder-icon">📁</span>
         <span className="folder-name">{name}</span>
       </div>
-=======
-      {name}
->>>>>>> bb07756ae2c81add885b1793835c58fbf6ca5391
     </div>
   );
 };
